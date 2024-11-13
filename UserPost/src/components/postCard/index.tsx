4348d8ceb822108b350b6810dd444b3ca970716e
Joshua-05 <danil_kuznecov3020@mail.ru>
2024-11-13 19:22:16 +0400

    create react project

---
+++ UserPost/src/components/postCard/index.tsx
@@ -0,0 +1,16 @@
+import style from "./style.module.css"
+
+const PostCard = () => {
+    return(
+        <div className={style.card}>
+            <div className={style.head}>
+                <p>Автор</p>
+            </div>
+            <div className={style.body}>
+
+            </div>
+        </div>
+    )
+}
+
+export default PostCard
---
+++ UserPost/src/components/postList/index.tsx
@@ -0,0 +1,12 @@
+import PostCard from "../postCard"
+
+const PostList = () => {
+    return(
+        <>
+            <p>Hi, world</p>
+            <PostCard />
+        </>
+    )
+}
+
+export default PostList
